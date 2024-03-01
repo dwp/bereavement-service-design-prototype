acf25735d862285f1fa4200c8db9a36b093daeef
benraine <benraine@users.noreply.github.com>
2024-03-01 13:55:54 +0000

    routes fix

--- app.js
+++ app.js
@@ -44,26 +44,7 @@ app.use(autoStoreDataMiddleware);
 
 // [Journey routes] (do not edit this comment, it is used in automation)
 app.use('/sprint-75/', setVersionMiddleware('/sprint-75/'), require('./routes/sprint-75'));
-app.use('/sprint-63/', setVersionMiddleware('/sprint-63/'), require('./routes/sprint-63'));
-app.use('/sprint-62-severity/', setVersionMiddleware('/sprint-62-severity/'), require('./routes/sprint-62-severity'));
-app.use('/sprint-62/', setVersionMiddleware('/sprint-62/'), require('./routes/sprint-62'));
-app.use('/sprint-61-1/', setVersionMiddleware('/sprint-61-1/'), require('./routes/sprint-61-1'));
-app.use('/sprint-61/', setVersionMiddleware('/sprint-61/'), require('./routes/sprint-61'));
-app.use('/sprint-60-1/', setVersionMiddleware('/sprint-60-1/'), require('./routes/sprint-60-1'));
-app.use('/sprint-60/', setVersionMiddleware('/sprint-60/'), require('./routes/sprint-60'));
-app.use('/sprint-59/', setVersionMiddleware('/sprint-59/'), require('./routes/sprint-59'));
-app.use('/sprint-58/', setVersionMiddleware('/sprint-58/'), require('./routes/sprint-58'));
-app.use('/sprint-57/', setVersionMiddleware('/sprint-57/'), require('./routes/sprint-57'));
-app.use('/sprint-56/', setVersionMiddleware('/sprint-56/'), require('./routes/sprint-56'));
-app.use('/sprint-55/', setVersionMiddleware('/sprint-55/'), require('./routes/sprint-55'));
-app.use('/sprint-54/', setVersionMiddleware('/sprint-54/'), require('./routes/sprint-54'));
-app.use('/sprint-53/', setVersionMiddleware('/sprint-53/'), require('./routes/sprint-53'));
-app.use('/sprint-48/', setVersionMiddleware('/sprint-48/'), require('./routes/sprint-48'));
-app.use('/sprint-47/', setVersionMiddleware('/sprint-47/'), require('./routes/sprint-47'));
-app.use('/v2-1-0/', setVersionMiddleware('/v2-1-0/'), require('./routes/v2-1-0'));
-app.use('/v2-0-0/', setVersionMiddleware('/v2-0-0/'), require('./routes/v2-0-0'));
-app.use('/v1-1-0/', setVersionMiddleware('/v1-1-0/'), require('./routes/v1-1-0'));
-app.use('/', require('./routes/v1-0-0'));
+
 
 // Request middleware - renders views for GET requests without controller
 app.get(/^([^.]+)$/, (req, res, next) => {
